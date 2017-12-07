b7abd387a660bb7d69482df481e81e12cb6dbff0
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-12-07 14:24:48 +0530

    (Green) - Implemented logic to return "FizzBuzz" for number divisible by 3 & 5

--- FizzBuzzKata/FizzBuzz.swift
+++ FizzBuzzKata/FizzBuzz.swift
@@ -10,7 +10,11 @@ class FizzBuzz {
     
     func takeInput(number: Int) -> String {
         if number != 0 {
-            if (number % 3) == 0 {
+            if (number % 3) == 0
+                && (number % 5) == 0 {
+                return "FizzBuzz"
+            }
+            else if (number % 3) == 0 {
                 return "Fizz"
             }
             else if (number % 5) == 0 {
